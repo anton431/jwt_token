a0721d7bb6040bdf1a9689dd16eaff9940bc7efc
anton <anton.dontsovgs@mail.ru>
2023-06-04 20:32:09 +0300

    jwt form

--- shift/serviceapp/views.py
+++ shift/serviceapp/views.py
@@ -1,13 +1,35 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from serviceapp.models import Person
+from serviceapp.serializer import TokenSerializer
+import requests
 
 
-class HelloView(APIView):
+class PersonInfoView(APIView):
     permission_classes = (IsAuthenticated,)
 
     def get(self, request):
         username = request.user.username
-        print(username)
-        content = {'message': 'Hello, World!'}
-        return Response({username: content})
+        pers = Person.objects.filter(login = username).first()
+        content = {'username': username,
+                   'salary': pers.salary,
+                   'date_up': pers.date_up}
+        return Response(content)
+
+class TokenView(APIView):
+    serializer_class = TokenSerializer
+
+    def post(self,request):
+        token = request.data['token']
+        header = {
+            "Authorization": "Bearer {}".format(token),
+            "Content-Type": "application/json",
+        }
+        response = requests.get(
+            url=f'http://127.0.0.1:8000/person/',
+            headers=header,
+            timeout=5
+        ).json()
+        return Response(response)
+
